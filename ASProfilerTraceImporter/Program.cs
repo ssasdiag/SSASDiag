5a6fc66bd393a0e32bf04be4f4cfa81607f253f3
jburchel <jburchel@microsoft.com>
2017-01-13 21:39:07 -0600

    Finally got all assemblies loading nicely without having to copy locally to dir, putting only in temp dir.

--- ASProfilerTraceImporter/Program.cs
+++ ASProfilerTraceImporter/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Reflection;
 using Microsoft.Win32;
 using System.IO;
 
@@ -17,47 +17,56 @@ namespace ASProfilerTraceImporter
         [STAThread]
         static void Main()
         {
-            Application.ApplicationExit += Application_ApplicationExit;
+            // yes it's a hack to get started...
+            if (AppDomain.CurrentDomain.BaseDirectory != Environment.GetEnvironmentVariable("temp") + "\\ASProfilerTraceImporter\\dll")
+            {
+                // Set file extension to the way we want to handle it
+                RegistryKey r;
+                if (Registry.ClassesRoot.OpenSubKey("SystemFileAssociations\\.trc\\Shell\\Import\\Command") == null)
+                    r = Registry.ClassesRoot.CreateSubKey("SystemFileAssociations").CreateSubKey(".trc").CreateSubKey("Shell").CreateSubKey("Import").CreateSubKey("Command", RegistryKeyPermissionCheck.ReadWriteSubTree);
+                else
+                    r = Registry.ClassesRoot.CreateSubKey("Software\\Classes\\ASProfilerTraceImporter\\shell\\import\\command", RegistryKeyPermissionCheck.ReadWriteSubTree);
+                r.SetValue("", "\"" + Application.ExecutablePath + "\" \"%1\"");
+                Registry.ClassesRoot.OpenSubKey("SystemFileAssociations\\.trc\\shell\\import", true).SetValue("", "&Import profiler trace to SQL table");
 
-            if (!Directory.Exists(Environment.GetEnvironmentVariable("temp") + "\\ASProfilerTraceImporter\\dll")) Directory.CreateDirectory(Environment.GetEnvironmentVariable("temp") + "\\ASProfilerTraceImporter\\dll");
-            Environment.CurrentDirectory = Environment.GetEnvironmentVariable("temp") + "\\ASProfilerTraceImporter";
+                if (!Directory.Exists(Environment.GetEnvironmentVariable("temp") + "\\ASProfilerTraceImporter\\dll"))
+                    Directory.CreateDirectory(Environment.GetEnvironmentVariable("temp") + "\\ASProfilerTraceImporter\\dll").Attributes = FileAttributes.Directory | FileAttributes.Hidden;
+                else
+                    foreach (string f in Directory.EnumerateFiles(Environment.GetEnvironmentVariable("temp") + "\\ASProfilerTraceImporter\\dll"))  File.Delete(f); 
 
-            AppDomainSetup ads = new AppDomainSetup();
-            ads.PrivateBinPathProbe = "dll";           
+                if (!File.Exists(Environment.GetEnvironmentVariable("temp") + "\\ASProfilerTraceImporter\\dll\\Microsoft.Data.ConnectionUI.dll")) File.WriteAllBytes(Environment.GetEnvironmentVariable("temp") + "\\ASProfilerTraceImporter\\dll\\Microsoft.Data.ConnectionUI.dll", Properties.Resources.Microsoft_Data_ConnectionUI);
+                if (!File.Exists(Environment.GetEnvironmentVariable("temp") + "\\ASProfilerTraceImporter\\dll\\Microsoft.Data.ConnectionUI.Dialog.dll")) File.WriteAllBytes(Environment.GetEnvironmentVariable("temp") + "\\ASProfilerTraceImporter\\dll\\Microsoft.Data.ConnectionUI.Dialog.dll", Properties.Resources.Microsoft_Data_ConnectionUI_Dialog);
+                if (!File.Exists(Environment.GetEnvironmentVariable("temp") + "\\ASProfilerTraceImporter\\dll\\Microsoft.SqlServer.ConnectionInfo.dll")) File.WriteAllBytes(Environment.GetEnvironmentVariable("temp") + "\\ASProfilerTraceImporter\\dll\\Microsoft.SqlServer.ConnectionInfo.dll", Properties.Resources.Microsoft_SqlServer_ConnectionInfo);
+                if (!File.Exists(Environment.GetEnvironmentVariable("temp") + "\\ASProfilerTraceImporter\\dll\\Microsoft.SqlServer.ConnectionInfoExtended.dll")) File.WriteAllBytes(Environment.GetEnvironmentVariable("temp") + "\\ASProfilerTraceImporter\\dll\\Microsoft.SqlServer.ConnectionInfoExtended.dll", Properties.Resources.Microsoft_SqlServer_ConnectionInfoExtended);
+                if (!File.Exists(Environment.GetEnvironmentVariable("temp") + "\\ASProfilerTraceImporter\\dll\\AS Profiler Trace Importer.exe")) File.Copy(Application.ExecutablePath, Environment.GetEnvironmentVariable("temp") + "\\ASProfilerTraceImporter\\dll\\AS Profiler Trace Importer.exe");
 
-            if (!Directory.Exists(Environment.CurrentDirectory + "\\dll")) Directory.CreateDirectory(Environment.CurrentDirectory + "\\dll");
-            if (!File.Exists(Environment.CurrentDirectory + "\\dll\\Microsoft.Data.ConnectionUI.dll")) File.WriteAllBytes(Environment.CurrentDirectory + "\\dll\\Microsoft.Data.ConnectionUI.dll", Properties.Resources.Microsoft_Data_ConnectionUI);
-            if (!File.Exists(Environment.CurrentDirectory + "\\dll\\Microsoft.Data.ConnectionUI.Dialog.dll")) File.WriteAllBytes(Environment.CurrentDirectory + "\\dll\\Microsoft.Data.ConnectionUI.Dialog.dll", Properties.Resources.Microsoft_Data_ConnectionUI_Dialog);
-            if (!File.Exists(Environment.CurrentDirectory + "\\dll\\Microsoft.SqlServer.ConnectionInfo.dll")) File.WriteAllBytes(Environment.CurrentDirectory + "\\dll\\SqlServer.ConnectionInfo.dll", Properties.Resources.Microsoft_SqlServer_ConnectionInfo);
-            if (!File.Exists(Environment.CurrentDirectory + "\\dll\\Microsoft.SqlServer.ConnectionInfoExtended.dll")) File.WriteAllBytes(Environment.CurrentDirectory + "\\dll\\Microsoft.SqlServer.ConnectionInfoExtended.dll", Properties.Resources.Microsoft_SqlServer_ConnectionInfoExtended);
+                var currentAssembly = Assembly.GetExecutingAssembly();
+                AppDomainSetup ads = new AppDomainSetup();
+                ads.ApplicationBase = Environment.GetEnvironmentVariable("temp") + "\\ASProfilerTraceImporter\\dll";
+                var otherDomain = AppDomain.CreateDomain("other domain", null, ads);
 
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+                var ret = otherDomain.ExecuteAssemblyByName(
+                                            currentAssembly.FullName,
+                                            new string[] { });
 
-            // Set file extension to the way we want to handle it
-            RegistryKey r;
-            if (Registry.ClassesRoot.OpenSubKey("SystemFileAssociations\\.trc\\Shell\\Import\\Command") == null)
-                r = Registry.ClassesRoot.CreateSubKey("SystemFileAssociations").CreateSubKey(".trc").CreateSubKey("Shell").CreateSubKey("Import").CreateSubKey("Command", RegistryKeyPermissionCheck.ReadWriteSubTree);
+                Environment.ExitCode = ret;
+                return;
+            }
             else
-                r = Registry.ClassesRoot.CreateSubKey("Software\\Classes\\ASProfilerTraceImporter\\shell\\import\\command", RegistryKeyPermissionCheck.ReadWriteSubTree);
-            r.SetValue("", "\"" + Application.ExecutablePath + "\" \"%1\"");
-            Registry.ClassesRoot.OpenSubKey("SystemFileAssociations\\.trc\\shell\\import", true).SetValue("", "&Import profiler trace to SQL table");
-
-            Application.ThreadException += Application_ThreadException;
-            try
             {
-                Application.Run(new frmProfilerTraceImporter());
-            }
-            catch (Exception e)
-            {
-                MessageBox.Show(e.ToString(), "Oops!  Exception...");
-            }
-        }
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
 
-        private static void Application_ApplicationExit(object sender, EventArgs e)
-        {
-            Environment.CurrentDirectory = Environment.GetEnvironmentVariable("temp");
-            Directory.Delete(Environment.GetEnvironmentVariable("temp") + "\\ASProfilerTraceImporter", true);
+                Application.ThreadException += Application_ThreadException;
+                try
+                {
+                    Application.Run(new frmProfilerTraceImporter());
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.ToString(), "Oops!  Exception...");
+                }
+            }       
         }
 
         static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
