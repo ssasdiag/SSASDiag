eaffc3e0f9b217a27fd24a50963235b50890cc60
jonburchel <jonburchel@gmail.com>
2018-09-28 18:34:04 -0500

    Updated About page for release.

--- SSASDiag/SSASDiag/frmAbout.Designer.cs
+++ SSASDiag/SSASDiag/frmAbout.Designer.cs
@@ -29,51 +29,78 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.lblVersion = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.LightSkyBlue;
-            this.label1.Location = new System.Drawing.Point(20, 69);
+            this.label1.Location = new System.Drawing.Point(95, 72);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(268, 300);
+            this.label1.Size = new System.Drawing.Size(237, 375);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Anne Zorner\r\nYinn Wong\r\nVan To\r\nMalcolm Stewart\r\nGangadhara Sadashivappa\r\nBeatric" +
-    "e Popa\r\nSamarendra Panda\r\nSrini Gajella\r\nGert Christen\r\nJon Burchel";
+            this.label1.Text = "Anne Zorner\r\nVan To\r\nGangadhara Sadashivappa\r\nBeatrice Popa\r\nSamarendra Panda\r\nGe" +
+    "rt Christen\r\n\r\n\r\nMalcolm Stewart\r\nSrini Gajella\r\nJon Burchel\r\n\r\n\r\nYinn Wong\r\nMel" +
+    "issa Street";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.label2.Location = new System.Drawing.Point(25, 9);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(263, 59);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Contributors";
             // 
             // lblVersion
             // 
             this.lblVersion.AutoSize = true;
-            this.lblVersion.Location = new System.Drawing.Point(124, 369);
+            this.lblVersion.Location = new System.Drawing.Point(184, 16);
             this.lblVersion.Name = "lblVersion";
             this.lblVersion.Size = new System.Drawing.Size(58, 13);
             this.lblVersion.TabIndex = 2;
-            this.lblVersion.Text = "v1.1.1.116";
+            this.lblVersion.Text = "v1.1.1.132";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.AliceBlue;
+            this.label2.Location = new System.Drawing.Point(138, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 32);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Design/Test";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.AliceBlue;
+            this.label3.Location = new System.Drawing.Point(129, 233);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(168, 32);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Development";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.AliceBlue;
+            this.label4.Location = new System.Drawing.Point(130, 362);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(167, 32);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Project Leads";
             // 
             // frmAbout
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.ClientSize = new System.Drawing.Size(312, 405);
-            this.Controls.Add(this.lblVersion);
+            this.ClientSize = new System.Drawing.Size(427, 462);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblVersion);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmAbout";
@@ -91,7 +118,9 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label lblVersion;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
