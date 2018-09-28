9528fbde6b00ac26286322495e9a84b7e2395cb5
jonburchel <jonburchel@gmail.com>
2018-09-28 18:17:50 -0500

    Updating version for new release.

--- SSASDiag/SSASDiag/Properties/AssemblyInfo.cs
+++ SSASDiag/SSASDiag/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SSAS Diagnostics Tool")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Creative Intelligence, for Microsoft")]
+[assembly: AssemblyCompany("Microsoft CSS")]
 [assembly: AssemblyProduct("SSASDiag")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.1.128")]
-[assembly: AssemblyFileVersion("1.1.1.128")]
+[assembly: AssemblyVersion("1.1.1.132")]
+[assembly: AssemblyFileVersion("1.1.1.132")]
